29e08f892d9e84ecc1d5db4bd038f55803968837
minhduc1997 <duc2521997@gmail.com>
2018-07-07 05:23:55 +0700

    commit

--- src/LunarCalanderVietNam.kt
+++ src/LunarCalanderVietNam.kt
@@ -22,10 +22,10 @@ class LunarCalendar{
         val T3 = T2 * T
         val dr = PI/180
         var Jd1 = 2415020.75933 + 29.53058868*k + 0.0001178*T2 - 0.000000155*T3
-        Jd1 = Jd1 + 0.00033*Math.sin((166.56 + 132.87*T - 0.009173*T2)*dr) // Mean new moon
-        val M = 359.2242 + 29.10535608*k - 0.0000333*T2 - 0.00000347*T3 // Sun's mean anomaly
-        val Mpr = 306.0253 + 385.81691806*k + 0.0107306*T2 + 0.00001236*T3 // Moon's mean anomaly
-        val F = 21.2964 + 390.67050646*k - 0.0016528*T2 - 0.00000239*T3 // Moon's argument of latitude
+        Jd1 = Jd1 + 0.00033*Math.sin((166.56 + 132.87*T - 0.009173*T2)*dr)
+        val M = 359.2242 + 29.10535608*k - 0.0000333*T2 - 0.00000347*T3
+        val Mpr = 306.0253 + 385.81691806*k + 0.0107306*T2 + 0.00001236*T3
+        val F = 21.2964 + 390.67050646*k - 0.0016528*T2 - 0.00000239*T3
         var C1=(0.1734 - 0.000393*T)*Math.sin(M*dr) + 0.0021*Math.sin(2*dr*M)
         C1 = C1 - 0.4068*Math.sin(Mpr*dr) + 0.0161*Math.sin(dr*2*Mpr)
         C1 = C1 - 0.0004*Math.sin(dr*3*Mpr)
@@ -43,16 +43,16 @@ class LunarCalendar{
     }
 
     fun getSunLongitude(jdn: Long, timeZone: Float): Double {
-        val T = (jdn - 2451545.5 - timeZone/24) / 36525 // Time in Julian centuries from 2000-01-01 12:00:00 GMT
+        val T = (jdn - 2451545.5 - timeZone/24) / 36525
         val T2 = T*T
         val dr = PI/180 // degree to radian
-        val M = 357.52910 + 35999.05030*T - 0.0001559*T2 - 0.00000048*T*T2 // mean anomaly, degree
-        val L0 = 280.46645 + 36000.76983*T + 0.0003032*T2 // mean longitude, degree
+        val M = 357.52910 + 35999.05030*T - 0.0001559*T2 - 0.00000048*T*T2
+        val L0 = 280.46645 + 36000.76983*T + 0.0003032*T2
         var DL = (1.914600 - 0.004817*T - 0.000014*T2)*Math.sin(dr*M)
         DL = DL + (0.019993 - 0.000101*T)*Math.sin(dr*2*M) + 0.000290*Math.sin(dr*3*M)
         var L = L0 + DL // true longitude, degree
         L = L*dr
-        L = L - PI*2*(Math.floor(L/(PI*2))) // Normalize to (0, 2*PI)
+        L = L - PI*2*(Math.floor(L/(PI*2)))
         return Math.floor(L / PI * 6)
     }
 
@@ -60,7 +60,7 @@ class LunarCalendar{
         val off = jdFromDate(31, 12, yy) - 2415021
         val k = Math.floor(off / 29.530588853)
         var nm = getNewMoonDay(k, timeZone)
-        val sunLong = getSunLongitude(nm, timeZone) // sun longitude at local midnight
+        val sunLong = getSunLongitude(nm, timeZone)
         if (sunLong >= 9) { nm = getNewMoonDay(k-1, timeZone)
         }
         return nm
@@ -69,7 +69,7 @@ class LunarCalendar{
     fun getLeapMonthOffset(a11: Long, timeZone: Float): Int{
         val k = Math.floor((a11 - 2415021.076998695) / 29.530588853 + 0.5)
         var last: Double
-        var i = 1 // We start with the month following lunar month 11
+        var i = 1
         var arc = getSunLongitude(getNewMoonDay(k + i, timeZone), timeZone)
         do {
             last = arc
@@ -79,7 +79,7 @@ class LunarCalendar{
         return i - 1
     }
 
-    fun convertSolar2Lunar(dd: Int, mm: Int, yy: Int, timeZone: Float){
+    fun convertSolar2Lunar(dd: Int, mm: Int, yy: Int, timeZone: Float): String{
         var lunarYear: Int
         val dayNumber = jdFromDate(dd, mm, yy)
         val k = Math.floor((dayNumber - 2415021.076998695) / 29.530588853)
@@ -116,7 +116,7 @@ class LunarCalendar{
         if (lunarMonth >= 11 && diff < 4) {
             lunarYear -= 1
         }
-        println("{'lunarDay': '$lunarDay', 'lunarMonth': '${lunarMonth.toInt()}, 'lunarYear': '$lunarYear'}")
+        return "{'lunarDay': '$lunarDay', 'lunarMonth': '${lunarMonth.toInt()}, 'lunarYear': '$lunarYear'}"
     }
 }
 
@@ -124,5 +124,5 @@ class LunarCalendar{
 
 fun main(args: Array<String>){
     val lunarCal =  LunarCalendar()
-    lunarCal.convertSolar2Lunar(5,7,2018,7f)
+    print(lunarCal.convertSolar2Lunar(7,7,2018,7f))
 }
